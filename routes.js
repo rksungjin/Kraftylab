b15f6aeaac75bd53d723c7c8deb3beac462c9480
Richard Kim <rk.sungjin@gmail.com>
2018-06-15 12:16:51 -0700

    routes updated

---
+++ routes.js
@@ -0,0 +1,13 @@
+module.exports = function(app){
+
+    var application = require('./routes/application');
+    var users = require('./routes/users');
+    //var trips = require('./routes/trips');
+    //var pricing = require('./routes/pricing');
+
+    app.use('/', application);
+    app.use('/users', users);
+    //app.use('/trips', trips);
+    //app.use('/pricing', pricing);
+//other routes..
+}
