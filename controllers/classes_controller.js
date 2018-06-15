a352f6ffd247dea9ec297d1104cbf561673af1a9
Shri <thisguyshri@gmail.com>
2018-06-14 21:16:26 -0700

    added routes

--- controllers/classes_controller.js
+++ controllers/classes_controller.js
@@ -1,31 +1,46 @@
-var db = require('../models');
 
 exports.index = function(req, res) {
 
-db.Classes.findAll({
-    where: {
-        ClassId: req.user.id
-    }
-}).then(function(classes) {
-    console.log(dbClasses);
-    res.render('classes/classes' , {
+    var db = require("../models");
 
-        layout: 'fill in modal',
-        class: dbClasses
-});
+module.exports = function(app) {
+  app.get("/api/class", function(req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.Classes.findAll({
+     include: [db.Classes]
+    }).then(function(dbClasses) {
+      res.json(dbClasses);
+    });
+  });
 
-});
+  app.get("/api/class/:class", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.Classes.findOne({
+      where: {
+        ClassID: req.params.id
+      },
+      include: [db.Classes]
+    }).then(function(dbClasses) {
+      res.json(dbClasses);
+    });
+  });
 
-};
-
-exports.createClass = function(req, res) {
-
-    console.log(req.user);
-
-    req.body.ClassId = req.user.id;
-
-    db.Classes.create(req.body).then(function(dbClasses) {
-        res.json(dbPost) // <--- ????
-    }
-
+  app.get("/api/class/:city", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.Classes.findOne({
+      where: {
+        CityID: req.params.id
+      },
+    
+    }).then(function(dbClasses) {
+      res.json(dbClasses);
+    });
+  });
+}
 }
