fef573175ad672884a1508da8d91bd9fc2f901dc
Jane Asawa <asawajane@gmail.com>
2018-06-15 15:05:22 -0700

    changed modal buttons for better input created js/kraftylab.js draft

--- js/kraftylab.js
+++ js/kraftylab.js
@@ -1,2 +1,46 @@
-// Krafty Lab javascript
+// Krafty Lab javascript for client-side javascript
 
+
+
+$(document).ready(function() {
+    var whatCity = "";
+    var whatClass = "";
+    $(".sip").on("click", function() {
+        whatCity = $(this).attr("data-name");
+        console.log("What city: " + whatCity);
+        whatClass = "sip";
+        console.log("What class: " + whatClass);
+        
+        $.get("/api/sipnsoap/whatCity", function(data) {
+            if (data.length !== 0) {
+                for (var i=0; i<data.length; i++) {
+                    var row = $("<div>");
+                    row.addClass("sip");
+                    row.append("<h4>" + "Sip 'N Soap Classes in " + whatCity + "</h4>");
+                    row.append("<p>" + data[i].date + "</p>");
+                    row.append("<p>" + data[i].time + "</p>");
+                    row.append("<p>" + data[i].address + "</p>");
+
+                    $("#clicksipnsoap").append(row);
+
+                }
+            }
+        })
+    })
+
+    $(".terr").on("click", function() {
+        whatCity = $(this).attr("data-name");
+        console.log("What city: " + whatCity);
+        whatClass = "terr";
+        console.log("What class: " + whatClass);   
+    })
+
+    $(".sws").on("click", function() {
+        whatCity = $(this).attr("data-name");
+        console.log("What city: " + whatCity);
+        whatClass = "sws";
+        console.log("What class: " + whatClass);   
+    })
+
+
+});
