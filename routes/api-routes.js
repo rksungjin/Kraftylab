4508d876aa3efff69bba9595379cb521d7136d33
Richard Kim <richard@Richards-MacBook-Pro.local>
2018-06-18 11:26:35 -0700

    model updated to sequelize with migration

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -29,13 +29,13 @@
 //         });
 //     });
         
-    //   class: req.body.class_name
-    // }, condition, function(result) {
-    //   if (result.changedRows == 0) {
-    //     // If no rows were changed, then the ID must not exist, so 404
-    //     return res.status(404).end();
-    //   } else {
-    //     res.status(200).end();
-    //   }
-    // });
+//       class: req.body.class_name
+//     }, condition, function(result) {
+//       if (result.changedRows == 0) {
+//         // If no rows were changed, then the ID must not exist, so 404
+//         return res.status(404).end();
+//       } else {
+//         res.status(200).end();
+//       }
+//     });
 //   });
